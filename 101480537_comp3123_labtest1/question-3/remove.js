e35d8fc4e639220b6fd94146ae659085a134ec41
Jonker88 <jonathan.h.cao88@gmail.com>
2025-10-02 14:14:49 -0400

    lab test

---
+++ 101480537_comp3123_labtest1/question-3/remove.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const logsDir = path.join(__dirname, 'Logs');
+
+
+if (fs.existsSync(logsDir)) {
+
+    fs.readdirSync(logsDir).forEach(file => {
+        console.log(`delete files...${file}`);
+        fs.unlinkSync(path.join(logsDir, file)); 
+    }); 
+
+
+fs.rmdirSync(logsDir);
+console.log('Logs directory deleted.')
+} else {
+    console.log('Logs directory does not exist');
+}
